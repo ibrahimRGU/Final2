810579cef9f124f94e84409d8e84ea0c1488c489
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-05-02 17:29:00 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 
 <?php
 session_start();
-
 ?>
 <?php
 include("connection.php"); //Establishing connection with our database
-
 $error = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
 {
@@ -17,23 +15,17 @@ if(isset($_POST["submit"]))
         ///// Define $username and $password
         $username=$_POST['username'];
         $password=$_POST['password'];
-
         //clean input photo user name
         $username = stripslashes( $username );
         $username=mysqli_real_escape_string($db,$username);
         $username = htmlspecialchars($username);
         $password=md5($password);
-
-
-
         //implement prepared statement to take care of sql injection and other vulnerabilities
-
         //declare instance of connection
         $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
         if (!($sqlcon->connect_errno)){
             echo"connection Failed";
         }
-
         //prepare statement
         if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=? and password=?")){
             //bind parameter
@@ -42,29 +34,17 @@ if(isset($_POST["submit"]))
             //get result
             $result = $stmt->get_result();
         }
-
-
         if( ($row=$result->fetch_row()))
         {
             $_SESSION['username'] = $username; // Initializing Session
             $_SESSION["userid"] = $row[0];//user id assigned to session global variable
             $_SESSION["timeout"] = time();//get session time: protects against session highjacking by logging off users or preventing users from access in time frame
             $_SESSION["ip"] = $_SERVER['REMOTE_ADDR'];// session highjacking:on login, the
-
-            if(isset($_SESSION['last_ip'])===false){
-                $_SESSION['last_ip'] = $_SERVER ['REMOTE_ADDR'];
-            }
-            if ($_SESSION['last_ip']!==$_SERVER['REMOTE_ADDR'])
-                session_unset();
-                session_destroy();
-
             header("location: photos.php"); // Redirecting To Other Page
         }else
         {
             $error = "Incorrect username or password.";
         }
-
     }
 }
-
 ?>
