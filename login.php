8e7271b6f447a0e93c4e95683b0ebfdd996cca45
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-05-02 17:49:23 +0100

    Update login.php

--- login.php
+++ login.php
@@ -41,12 +41,7 @@ if(isset($_POST["submit"]))
             $_SESSION["timeout"] = time();//get session time: protects against session highjacking by logging off users or preventing users from access in time frame
             $_SESSION["ip"] = $_SERVER['REMOTE_ADDR'];// session highjacking:on login, the
             //header("location: photos.php"); // Redirecting To Other Page
-             ini_set('session.coockie_httponly',true);
-            $_SESSION['username'] = $username; // Initializing Session
-            $_SESSION["userid"] = $row[0];//user id assigned to session global variable
-            $_SESSION["timeout"] = time();//get session time: protects against session highjacking by logging off users or preventing users from access in time frame
-            $_SESSION["ip"] = $_SERVER['REMOTE_ADDR'];// session highjacking:on login, the
-             
+           
 
              if(isset($_SESSION['last_ip'])===false){
                 $_SESSION['last_ip'] = $_SERVER ['REMOTE_ADDR'];
